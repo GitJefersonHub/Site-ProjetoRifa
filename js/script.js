95bdc8bac9d80064af84b01c00e97966097d39f6
“GitJefersonHub” <jefersonalves.ti@gmail.com>
2025-09-21 20:02:14 -0300

    2002

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@ let valorArrecadado = 0; // Inicializa o valor total arrecadado.
 
 // Lista de números confirmados com nomes 
 let numerosConfirmados = {
-    9: "xxx"
+    1: "xxx", 2: "xxx", 3: "xxx", 4: "xxx", 5: "xxx", 6: "xxx", 7: "xxx", 8: "xxx", 9: "xxx", 10: "xxx",
+    11: "xxx", 12: "xxx", 13: "xxx", 14: "xxx", 15: "xxx", 16: "xxx", 17: "xxx", 18: "xxx", 19: "xxx", 20: "xxx",
+    21: "xxx", 22: "xxx", 23: "xxx", 24: "xxx", 25: "xxx", 26: "xxx", 27: "xxx", 28: "xxx", 29: "xxx", 30: "xxx",
+    //31: "xxx", 32: "xxx", 33: "xxx", 34: "xxx", 35: "xxx", 36: "xxx", 37: "xxx", 38: "xxx", 39: "xxx", 40: "xxx",
+    //41: "xxx", 42: "xxx", 43: "xxx", 44: "xxx", 45: "xxx", 46: "xxx", 47: "xxx", 48: "xxx", 49: "xxx", 50: "xxx",
+    //51: "xxx", 52: "xxx", 53: "xxx", 54: "xxx", 55: "xxx", 56: "xxx", 57: "xxx", 58: "xxx", 59: "xxx", 60: "xxx",
+    //61: "xxx", 62: "xxx", 63: "xxx", 64: "xxx", 65: "xxx", 66: "xxx", 67: "xxx", 68: "xxx", 69: "xxx", 70: "xxx",
+    //71: "xxx", 72: "xxx", 73: "xxx", 74: "xxx", 75: "xxx", 76: "xxx", 77: "xxx", 78: "xxx", 79: "xxx", 80: "xxx",
+    //81: "xxx", 82: "xxx", 83: "xxx", 84: "xxx", 85: "xxx", 86: "xxx", 87: "xxx", 88: "xxx", 89: "xxx", 90: "xxx",
+    //91: "xxx", 92: "xxx", 93: "xxx", 94: "xxx", 95: "xxx", 96: "xxx", 97: "xxx", 98: "xxx", 99: "xxx", 100: "xxx"
 }; // Objeto para armazenar números e seus respectivos nomes. 
 
 // Atualiza os valores exibidos na página e calcula os ganhos 
@@ -13,12 +22,12 @@ function atualizarValores() {
     valorArrecadado = Object.keys(numerosConfirmados).length * valorPorNumero;
 
     // Calcula os valores das premiações com base nas porcentagens 
-    const quintoGanhador = valorArrecadado * 0.0291; // 2,91% do total arrecadado. 
-    const quartoGanhador = valorArrecadado * 0.0459; // 4,59%. 
-    const terceiroGanhador = valorArrecadado * 0.0629; // 6,29%. 
-    const segundoGanhador = valorArrecadado * 0.08237; // 8,237%. 
-    const primeiroGanhador = valorArrecadado * 0.10514; // 10,514%. 
-    const manutencao = valorArrecadado * 0.0699; // 6,99% destinados à manutenção. 
+    const quintoGanhador = valorArrecadado * 0.04;
+    const quartoGanhador = valorArrecadado * 0.06;
+    const terceiroGanhador = valorArrecadado * 0.08;
+    const segundoGanhador = valorArrecadado * 0.10;
+    const primeiroGanhador = valorArrecadado * 0.12;
+    const manutencao = valorArrecadado * 0.03; //destinados à manutenção. 
 
     // Calcula o valor restante para o cliente após deduzir premiações e manutenção 
     const cliente = valorArrecadado - (
@@ -84,8 +93,8 @@ for (let i = 1; i <= 40; i++) { // Loop para criar botões de 1 a 40.
     // Adiciona um evento de clique ao botão 
     button.addEventListener('click', () => {
 
-        alert('Programa parado para manutenção!'); // Pausa a seleção de números.
-        //const confirmacao = confirm(` ${i}. Deseja continuar?`); // Pausa o envio da mensagem para o WhatsApp. 
+        alert('Siga o passo a passo no WhatsApp, aguarde a confirmação e boa sorte!'); // Pausa a seleção de números.
+        const confirmacao = confirm(` ${i}. Deseja continuar?`); // Pausa o envio da mensagem para o WhatsApp. 
 
         if (confirmacao) {
             const numeroWhatsApp = "5562981208278"; // Número de WhatsApp para envio. 
